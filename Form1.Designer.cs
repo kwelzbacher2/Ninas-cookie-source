814ccb3eab393de903d8700a8b506ba782b73546
K Welzbacher <35229915+kwelzbacher2@users.noreply.github.com>
2018-01-09 11:28:15 -0500

    Add files via upload
    source files from visual studio project

---
+++ Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace NinasCookieSource
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cookieListBox = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.waferRadioButton = new System.Windows.Forms.RadioButton();
+            this.oatmealRadioButton = new System.Windows.Forms.RadioButton();
+            this.chocoChipRadioButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.deliveryLabel = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.accumulateTextBox = new System.Windows.Forms.TextBox();
+            this.cookieTextBox = new System.Windows.Forms.TextBox();
+            this.buyButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cookies";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cookieListBox);
+            this.groupBox3.Location = new System.Drawing.Point(119, 20);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(93, 85);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Quantity";
+            // 
+            // cookieListBox
+            // 
+            this.cookieListBox.FormattingEnabled = true;
+            this.cookieListBox.Location = new System.Drawing.Point(6, 20);
+            this.cookieListBox.Name = "cookieListBox";
+            this.cookieListBox.Size = new System.Drawing.Size(80, 43);
+            this.cookieListBox.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.waferRadioButton);
+            this.groupBox2.Controls.Add(this.oatmealRadioButton);
+            this.groupBox2.Controls.Add(this.chocoChipRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(7, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(106, 100);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Choices";
+            // 
+            // waferRadioButton
+            // 
+            this.waferRadioButton.AutoSize = true;
+            this.waferRadioButton.Location = new System.Drawing.Point(7, 68);
+            this.waferRadioButton.Name = "waferRadioButton";
+            this.waferRadioButton.Size = new System.Drawing.Size(88, 17);
+            this.waferRadioButton.TabIndex = 2;
+            this.waferRadioButton.TabStop = true;
+            this.waferRadioButton.Text = "Vanilla Wafer";
+            this.waferRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // oatmealRadioButton
+            // 
+            this.oatmealRadioButton.AutoSize = true;
+            this.oatmealRadioButton.Location = new System.Drawing.Point(7, 44);
+            this.oatmealRadioButton.Name = "oatmealRadioButton";
+            this.oatmealRadioButton.Size = new System.Drawing.Size(64, 17);
+            this.oatmealRadioButton.TabIndex = 1;
+            this.oatmealRadioButton.TabStop = true;
+            this.oatmealRadioButton.Text = "Oatmeal";
+            this.oatmealRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // chocoChipRadioButton
+            // 
+            this.chocoChipRadioButton.AutoSize = true;
+            this.chocoChipRadioButton.Location = new System.Drawing.Point(7, 20);
+            this.chocoChipRadioButton.Name = "chocoChipRadioButton";
+            this.chocoChipRadioButton.Size = new System.Drawing.Size(97, 17);
+            this.chocoChipRadioButton.TabIndex = 1;
+            this.chocoChipRadioButton.TabStop = true;
+            this.chocoChipRadioButton.Text = "Chocolate Chip";
+            this.chocoChipRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nina\'s Cookie Source";
+            // 
+            // deliveryLabel
+            // 
+            this.deliveryLabel.AutoSize = true;
+            this.deliveryLabel.Location = new System.Drawing.Point(46, 215);
+            this.deliveryLabel.Name = "deliveryLabel";
+            this.deliveryLabel.Size = new System.Drawing.Size(0, 13);
+            this.deliveryLabel.TabIndex = 2;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.accumulateTextBox);
+            this.groupBox4.Controls.Add(this.cookieTextBox);
+            this.groupBox4.Location = new System.Drawing.Point(13, 242);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 91);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Cost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Accumulated:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "For chosen cookies:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // accumulateTextBox
+            // 
+            this.accumulateTextBox.Location = new System.Drawing.Point(119, 54);
+            this.accumulateTextBox.Name = "accumulateTextBox";
+            this.accumulateTextBox.Size = new System.Drawing.Size(75, 20);
+            this.accumulateTextBox.TabIndex = 1;
+            // 
+            // cookieTextBox
+            // 
+            this.cookieTextBox.Location = new System.Drawing.Point(119, 19);
+            this.cookieTextBox.Name = "cookieTextBox";
+            this.cookieTextBox.Size = new System.Drawing.Size(75, 20);
+            this.cookieTextBox.TabIndex = 0;
+            // 
+            // buyButton
+            // 
+            this.buyButton.Location = new System.Drawing.Point(55, 339);
+            this.buyButton.Name = "buyButton";
+            this.buyButton.Size = new System.Drawing.Size(75, 23);
+            this.buyButton.TabIndex = 0;
+            this.buyButton.Text = "Buy Cookies";
+            this.buyButton.UseVisualStyleBackColor = true;
+            this.buyButton.Click += new System.EventHandler(this.buyButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(201, 339);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(351, 339);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(301, 292);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.ReadOnly = true;
+            this.nameTextBox.Size = new System.Drawing.Size(144, 20);
+            this.nameTextBox.TabIndex = 7;
+            this.nameTextBox.Text = "Author: Katie Welzbacher";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(256, 47);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 8;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(256, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Select Order Date:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 393);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.buyButton);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.deliveryLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Cookies";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox cookieListBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton waferRadioButton;
+        private System.Windows.Forms.RadioButton oatmealRadioButton;
+        private System.Windows.Forms.RadioButton chocoChipRadioButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label deliveryLabel;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox accumulateTextBox;
+        private System.Windows.Forms.TextBox cookieTextBox;
+        private System.Windows.Forms.Button buyButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿//Programmer: Katie Welzbacher
+//Date: 5/3/17
+//Lab 10 Ch. 12
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NinasCookieSource
+{
+    public partial class Form1 : Form
+    {
+        //declare variables
+        double accumulate;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //add to the ListBox
+            this.cookieListBox.Items.AddRange(new Object[]{
+                "1/2 dozen",
+                "1 dozen",
+                "2 dozen",
+                "3 dozen"
+            });
+            //set the minimum calendar date to today
+            monthCalendar1.MinDate = DateTime.Today;
+            deliveryLabel.Text = "Cookies delivered on: " + monthCalendar1.SelectionStart.AddDays(3).ToShortDateString();
+
+            //create event handler for when radio button is changed
+            chocoChipRadioButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            oatmealRadioButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            waferRadioButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            cookieListBox.SelectedIndexChanged += new EventHandler(radioButtons_CheckedChanged);
+
+        }
+
+        //calculate the price when buy button clicked
+        private void buyButton_Click(object sender, EventArgs e)
+        {
+            //declare variables
+            double price=0;
+
+            //try and catch exceptions
+            try
+            {
+                if ((chocoChipRadioButton.Checked == false) && (oatmealRadioButton.Checked == false) && (waferRadioButton.Checked == false))
+                {
+                    throw new ArgumentException();
+                }
+                try
+                    {
+                        if (cookieListBox.SelectedIndex == -1) throw new ArgumentException();
+                        //give price based on what items chosen
+                        if (oatmealRadioButton.Checked == true)
+                        {
+
+
+                            if (cookieListBox.SelectedIndex == 0)
+                            {
+                                price = 3.49;
+                            }
+                            else if (cookieListBox.SelectedIndex == 1)
+                            {
+                                price = 6.98;
+                            }
+                            else if (cookieListBox.SelectedIndex == 2)
+                            {
+                                price = 13.96;
+                            }
+                            else
+                            {
+                                price = 20.94;
+                            }
+                        }
+                        else if (chocoChipRadioButton.Checked == true)
+                        {
+
+
+                            if (cookieListBox.SelectedIndex == 0)
+                            {
+                                price = 4.49;
+                            }
+                            else if (cookieListBox.SelectedIndex == 1)
+                            {
+                                price = 8.98;
+                            }
+                            else if (cookieListBox.SelectedIndex == 2)
+                            {
+                                price = 17.96;
+                            }
+                            else
+                            {
+                                price = 26.94;
+                            }
+                        }
+                        else
+                        {
+                            if (cookieListBox.SelectedIndex == 0)
+                            {
+                                price = 3.24;
+                            }
+                            else if (cookieListBox.SelectedIndex == 1)
+                            {
+                                price = 6.48;
+                            }
+                            else if (cookieListBox.SelectedIndex == 2)
+                            {
+                                price = 12.96;
+                            }
+                            else
+                            {
+                                price = 19.44;
+                            }
+
+                        }
+                        //display price and calculate accumulate
+                        cookieTextBox.Text = price.ToString("c");
+                        accumulate += price;
+                        accumulateTextBox.Text = accumulate.ToString("c");
+                    }
+                    catch (ArgumentException iee)
+                    {
+                        MessageBox.Show("Choose a quantity of cookies.", "Missing purchase detail.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                
+            } catch (ArgumentException ie)
+            {
+                MessageBox.Show("Choose a type of cookie.", "Missing purchase detail.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+        // show the delivery date on the label
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            const int DAYS_TO_ADD = 3;
+            deliveryLabel.Text = "Cookies delivered on: " + monthCalendar1.SelectionStart.AddDays(DAYS_TO_ADD).ToShortDateString();
+        }
+
+        //clear the price textbox when different button chosen
+        private void radioButtons_CheckedChanged (object sender, EventArgs e)
+        {
+            cookieTextBox.Clear();
+        }
+
+        //close the form
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        //clear the form
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            cookieTextBox.Clear();
+            accumulateTextBox.Clear();
+            cookieListBox.SelectedIndex = -1;
+            chocoChipRadioButton.Checked = false;
+            oatmealRadioButton.Checked = false;
+            waferRadioButton.Checked = false;
+            monthCalendar1.ShowToday = true;
+            deliveryLabel.Text = "Cookies delivered on: " + DateTime.Today.AddDays(3).ToShortDateString();
+
+        }
+
+       
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
